ca7964adedb9970ffaeb47aa6acc45af28c44f2e
Vincenzo Maio <vincenzo.maio@motork.io>
2023-03-21 16:21:42 +0100

    1st commit

---
+++ src/main/java/org/vikviper/WellDemo/WellDemoApplication.java
@@ -0,0 +1,13 @@
+package org.vikviper.WellDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vikviper/WellDemo/WellDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vikviper.WellDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
